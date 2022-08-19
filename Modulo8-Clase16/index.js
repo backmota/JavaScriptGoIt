d1b435d9f48ea13ce464dff6e07e3ca5f55bd49e
José Juan Mota <backmota@DarkSideOfTheCode.local>
2022-08-18 20:03:23 -0500

    JSON

--- Modulo8-Clase16/index.js
+++ Modulo8-Clase16/index.js
@@ -1,2 +1,12 @@
-index.js
+const food = {
+	name: "clamchowder",
+	price: 300,
+	availability: true,
+	allergy: "seafood",
+};
 
+console.log(food);
+
+const foodJSON = JSON.stringify(food);
+
+console.log(foodJSON);
