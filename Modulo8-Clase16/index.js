7a7958c05881bc3aa35fe681d46d3a33eb7ab159
José Juan Mota <backmota@DarkSideOfTheCode.local>
2022-08-18 20:20:39 -0500

    JSON

--- Modulo8-Clase16/index.js
+++ Modulo8-Clase16/index.js
@@ -5,8 +5,31 @@ const food = {
 	allergy: "seafood",
 };
 
+const json = '{"name":"Mango","age":3,"isHappy":true}';
+
 console.log(food);
 
 const foodJSON = JSON.stringify(food);
 
 console.log(foodJSON);
+
+console.log(json);
+const user = JSON.parse(json);
+console.log(user);
+console.log(user.name);
+
+user.student = true;
+
+console.log(user.student);
+
+const dataJSON = "No soy un JSON";
+
+try{
+	const data = JSON.parse('{username": "Mango"}');
+}catch (error){
+	console.log(error);
+	console.log(error.name);
+	console.log(error.message);
+}
+
+console.log("✅ This is fine, we handled parse error in try...catch");
